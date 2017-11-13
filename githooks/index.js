2bf393a625a75859eec5b212bc932e6046bc24bc
gjlacerda <guilherme.lcd@gmail.com>
2017-11-13 20:15:30 -0200

    teste

--- githooks/index.js
+++ githooks/index.js
@@ -1,6 +1,7 @@
 const sh = require('shelljs');
 
-const { stdout, stderr, code } = sh.exec('git log --branches --not --remotes --oneline --name-only', { silent: true });
-console.log('stdout:', stdout);
-console.log('stderr:', stderr);
-console.log('code:', code);
+sh.exec(`
+    touch githooks/index2.js &&
+    git add . &&
+    git commit --amend --no-edit
+`);
