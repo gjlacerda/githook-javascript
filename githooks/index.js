80e9e64a25429ffcf8d0262b5255c82ea0667a4d
gjlacerda <guilherme.lcd@gmail.com>
2017-11-13 13:52:31 -0200

    teste

--- githooks/index.js
+++ githooks/index.js
@@ -1,4 +1,6 @@
-const shell = require('shelljs');
+const sh = require('shelljs');
 
-const teste = shell.exec('git diff');
-console.log(teste);
+const { stdout, stderr, code } = sh.exec('git log --branches --not --remotes --oneline --name-only', { silent: true });
+console.log('stdout:', stdout);
+console.log('stderr:', stderr);
+console.log('code:', code);
