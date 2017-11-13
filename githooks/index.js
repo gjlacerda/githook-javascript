653d28932f1a29da341119e2b6546d6d107e7616
gjlacerda <guilherme.lcd@gmail.com>
2017-11-13 20:38:15 -0200

    Forced error

--- githooks/index.js
+++ githooks/index.js
@@ -1,3 +1,9 @@
 const sh = require('shelljs');
 
-sh.exit(1);
+const colorRed = '$(tput setaf 1)'
+const {stdout} = sh.exec('git log --branches --not --remotes --oneline --name-only', {silent: true});
+
+if (stdout.includes('index.js')) {
+    sh.message(`${colorRed}You can't change index.js file`);
+    sh.exit(1);
+}
